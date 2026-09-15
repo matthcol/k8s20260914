9a232434002f234617d572bbee7631d32424723a
matthcol <matthias.colin@gmail.com>
2026-09-15 14:02:06 +0200

    SQL init with CM

--- cinema-stack/deploy-stack.ps1
+++ cinema-stack/deploy-stack.ps1
@@ -1,5 +1,13 @@
 $NAMESPACE="cinema"
+# TODO: check NS not empty and not existing
+
 kubectl create ns $NAMESPACE
 kubectl config set-context --current --namespace $NAMESPACE
 
+
+kubectl apply -f db-config.configmap.yml
+kubectl apply -f db-init.configmap.yml
+
+kubectl apply -f db.statefulset.yml
 kubectl apply -f api.deployment.yml
+
